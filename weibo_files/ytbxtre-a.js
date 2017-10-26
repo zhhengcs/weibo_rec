43d52214e973ad2c20afa1998917332aae583fa3
heng zhang <heng zhang>
2017-10-26 23:35:21 +0800

    origin files

---
+++ weibo_files/ytbxtre-a.js
@@ -0,0 +1,17 @@
+//--------------------------------------------------------------------------------------------
+
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-69162731-1']);
+      _gaq.push(['_trackPageview']);
+      
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-72184248-1']);
+      _gaq.push(['_trackPageview']);      
+
+      (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+//--------------------------------------------------------------------------------------------
